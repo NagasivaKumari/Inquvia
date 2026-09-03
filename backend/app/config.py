5f95195d687620a6711609fe4fab4079e4d6f4b0
Nagasiva Kumari <nagashivakota@gmail.com>
2026-09-03 14:39:12 +0530

    chore: set challenge tag to inquvia

--- backend/app/config.py
+++ backend/app/config.py
@@ -26,7 +26,7 @@ MONGODB_DB_NAME = os.getenv("MONGODB_DB_NAME", "inquvia")
 ALGORAND_NETWORK = os.getenv("NEXT_PUBLIC_ALGORAND_NETWORK", None) or os.getenv("ALGORAND_NETWORK", "mainnet")
 ALGORAND_USDC_ASA = os.getenv("ALGORAND_USDC_ASA", "31566704")
 X402_FACILITATOR_URL = os.getenv("X402_FACILITATOR_URL", "https://facilitator.goplausible.com")
-X402_CHALLENGE_TAG = os.getenv("X402_CHALLENGE_TAG", "x402-global-challenge")
+X402_CHALLENGE_TAG = os.getenv("X402_CHALLENGE_TAG", "inquvia")
 INQUVIA_PAYTO_ADDRESS = os.getenv("INQUVIA_PAYTO_ADDRESS", "").strip()
 SERVER_WALLET_MNEMONIC = os.getenv("SERVER_WALLET_MNEMONIC", "")
 
